dfb924bf76d1306db6d3e5b1e3b6b3c8f6c41a4d
farahBaradi <farah.barade@gmail.com>
2022-05-06 19:59:51 +0200

    particip + coup very good

--- clientTCP.c
+++ clientTCP.c
@@ -4,261 +4,269 @@
 int main(int argc, char **argv)
 {
 
-  int sock, /* descripteur de la socket locale */
-      port, /* variables de lecture */
-      err,
-      choix = 1;
+int sock, /* descripteur de la socket locale */
+port, /* variables de lecture */
+err;
 
-  char chaine[100], dem, *nomMachServ;
+char chaine[100] = "" , dem, *nomMachServ;
 
-  TPartieReq req;            // Participation request
-  TPartieRep rep;            // Answer to the participation request
-  TCase square;              // Case
-  TCoupReq playRequest;      // Play request
-  TCoupRep ownResponse;      // Response to the play request
-  TCoupRep opponentResponse; // delete
+TPartieReq req; // Participation request
+TPartieRep rep; // Answer to the participation request
+TCase square; // Case
+TCoupReq playRequest; // Play request
+TCoupRep ownResponse; // Response to the play request
+TCoupRep opponentResponse; // delete
 
-  // verification des arguments
-  if (argc != 3)
-  {
-    printf("usage : %s nom/IPServ port\n", argv[0]);
-    return -1;
-  }
+// verification des arguments
+if (argc != 3)
+{
+printf("usage : %s nom/IPServ port\n", argv[0]);
+return -1;
+}
 
-  nomMachServ = argv[1];
-  port = atoi(argv[2]);
-  sock = socketClient(nomMachServ, port);
+nomMachServ = argv[1];
+port = atoi(argv[2]);
+sock = socketClient(nomMachServ, port);
 
-  /////////////////////////////////////// ASK FOR PARTICIPATION ///////////////////////////////////////////////////////////////////////
+/////////////////////////////////////// ASK FOR PARTICIPATION ///////////////////////////////////////////////////////////////////////
 
-  printf("(client) tapez p pour participer ainsi votre nom ou login : ");
-  scanf("%c %s", &dem, chaine);
-  printf("(client) envoi de - %s - \n", chaine);
+printf("(client) tapez p pour participer ainsi votre nom ou login : ");
+scanf("%c %s", &dem, chaine);
+printf("(client) envoi de - %s - \n", chaine);
 
-  switch (dem)
-  {
-  case 'p':
-    req.idRequest = PARTIE;
-    break;
-  default:
-    break;
-  }
+switch (dem)
+{
+case 'p':
+req.idRequest = PARTIE;
+break;
+default:
+break;
+}
 
-  // Requesting to play
-  memcpy(req.nomJoueur, chaine, strlen(chaine));
-  printf("(client) send nom est %s \n", req.nomJoueur);
-  err = send(sock, &req, sizeof(TPartieReq), 0);
-  if (err <= 0)
-  {
-    perror("(client) erreur sur le send");
-    shutdown(sock, SHUT_RDWR);
-    close(sock);
-  }
+// Requesting to play
+memcpy(req.nomJoueur, chaine, strlen(chaine)+ 1 );
+printf("(client) send nom est %s \n", req.nomJoueur);
+err = send(sock, &req, sizeof(TPartieReq), 0);
+if (err <= 0)
+{
+perror("(client) erreur sur le send");
+shutdown(sock, SHUT_RDWR);
+close(sock);
+}
 
-  // Receive anwser from server
-  err = recv(sock, &rep, sizeof(TPartieRep), 0);
-  if (err <= 0)
-  {
-    perror("(Client) erreur dans la reception");
-    close(sock);
-    return -6;
-  }
+// Receive anwser from server
+err = recv(sock, &rep, sizeof(TPartieRep), 0);
+if (err <= 0)
+{
+perror("(Client) erreur dans la reception");
+close(sock);
+return -6;
+}
 
-  switch (rep.err)
-  {
-  case ERR_OK:
-    if (rep.coul == BLANC)
-    {
-      printf("(Client) vous etes le jouer blanc et le nom de votre adverse c'est : %s\n", rep.nomAdvers);
-      break;
-    }
-    if (rep.coul == NOIR)
-    {
-      printf("(Client) vous etes le jouer noir et le nom de votre adverse c'est : %s\n", rep.nomAdvers);
-      break;
-    }
-  case ERR_PARTIE:
-    printf("pas possible de participer !\n");
-    break;
-  default:
-    break;
-  }
-  printf("\n");
+switch (rep.err)
+{
+case ERR_OK:
+if (rep.coul == BLANC)
+{
+printf("(Client) vous etes le jouer blanc et le nom de votre adverse c'est : %s\n", rep.nomAdvers);
+break;
+}
+if (rep.coul == NOIR)
+{
+printf("(Client) vous etes le jouer noir et le nom de votre adverse c'est : %s\n", rep.nomAdvers);
+break;
+}
+case ERR_PARTIE:
+printf("pas possible de participer !\n");
+break;
+default:
+break;
+}
+printf("\n");
 
-  /////////////////////////////////////// FINISH OF ASKING PARTICIPATION //////////////////////////////////////////////////////////////
+/////////////////////////////////////// FINISH OF ASKING PARTICIPATION //////////////////////////////////////////////////////////////
 
-  if (rep.coul == BLANC)
-  {
-    printf("(Client) you are the first player! lancer un coup\n");
-    // Arrange Play protocol package
-    // TODO ask the IA for a coup and get the result
-    playRequest.idRequest = COUP;
-    playRequest.typeCoup = POS_PION;
-    playRequest.coul = BLANC;
-    square.lg = TROIS;
-    square.col = B;
-    playRequest.action.posPion = square;
-    // Send play
-    err = send(sock, &playRequest, sizeof(TCoupReq), 0);
-    if (err <= 0)
-    {
-      perror("(client) erreur sur le send de coup ");
-      shutdown(sock, SHUT_RDWR);
-      close(sock);
-    }
-    printf("(client) send demande coup de jouer 1 OK \n");
-    // Receive validation of own play or adversarie
-    err = recv(sock, &ownResponse, sizeof(TCoupRep), 0);
-    if (err <= 0)
-    {
-      perror("(Client) erreur dans la reception de resultats coup ");
-      close(sock);
-      return -20;
-    }
+if (rep.coul == BLANC)
+{
+printf("(Client) you are the first player! lancer un coup\n");
+// Arrange Play protocol package
+// TODO ask the IA for a coup and get the result
+playRequest.idRequest = COUP;
+playRequest.typeCoup = POS_PION;
+playRequest.coul = rep.coul;
+square.lg = DEUX;
+square.col = C;
+playRequest.action.posPion = square;
+// Send play
+err = send(sock, &playRequest, sizeof(TCoupReq), 0);
+if (err <= 0)
+{
+perror("(client) erreur sur le send de coup ");
+shutdown(sock, SHUT_RDWR);
+close(sock);
+}
+// Receive validation of own play
+err = recv(sock, &ownResponse, sizeof(TCoupRep), 0);
+if (err <= 0)
+{
+perror("(Client) erreur dans la reception de resultats coup ");
+close(sock);
+return -20;
+}
+printf("(cclient) BLANC send play - first play!!!\n");
+printf("\n");
+// Treat Play Response
+switch (ownResponse.err)
+{
+case ERR_OK:
+if (ownResponse.validCoup == VALID)
+{
+printf("(Client) coup bien jouee!! \n");
+if (ownResponse.propCoup == CONT)
+{
+printf("(Client) votre coup est valid et aucun gagne pour le moment! \n");
+}
+else if (ownResponse.propCoup == GAGNE)
+{
+printf("(Client) you are winning! \n");
+}
+}
+else
+{
+// TODO treat TIMEOUT AND TRICHE
+}
+break;
+case ERR_COUP:
+printf("tu ne peut pas jouer un coup avant de participer !\n");
+break;
+default:
+break;
+}
+}
 
-    // Treat Play Response
-    switch (ownResponse.err)
-    {
-    case ERR_OK:
-      if (ownResponse.validCoup == VALID)
-      {
-        printf("(Client) coup bien jouee!! \n");
-        if (ownResponse.propCoup == CONT)
-        {
-          printf("(Client) votre coup est valid et aucun gagne pour le moment! \n");
-        }
-        if (ownResponse.propCoup == GAGNE)
-        {
-          printf("(Client) you are wenning! \n");
-        }
-      }
-      else
-      {
-        // TODO treat TIMEOUT AND TRICHE
-      }
-      break;
-    case ERR_COUP:
-      printf("tu ne peut pas jouer un coup avant de participer !\n");
-      break;
-    default:
-      break;
-    }
-  }
+// Start to send plays, runs till match is gange, perdu, triche, timeout
+while (1)
+{
 
-  // Start to send plays, runs till match is gange, perdu, triche, timeout
-  while (true)
-  {
+// Receive validation of adversaire play
+err = recv(sock, &opponentResponse, sizeof(TCoupRep), 0);
+if (err <= 0)
+{
+perror("(Client) erreur dans la reception de resultats coup ");
+close(sock);
+return -20;
+}
+printf("(client) Received validation of adversaire play \n");
 
-    // Receive validation of adversaire play
-    err = recv(sock, &opponentResponse, sizeof(TCoupRep), 0);
-    if (err <= 0)
-    {
-      perror("(Client) erreur dans la reception de resultats coup ");
-      close(sock);
-      return -20;
-    }
+// Treat adversaire play
+switch (opponentResponse.err)
+{
+case ERR_OK:
+if (opponentResponse.validCoup == VALID)
+{
+printf("(Client) coup adverse bien jouee!! \n");
+if (playRequest.typeCoup == POS_PION)
+{
+printf("(Client) il a fait un pos \n");
+}
+else if (opponentResponse.propCoup == CONT)
+{
+printf("(Client) aucun gagne pour le moment! \n");
+}
+else if (opponentResponse.propCoup == GAGNE)
+{
+printf("(Client) the adverse is wenning! \n");
+}
+}
+else
+{
+printf("(Client) Your opponent made a mistake!! \n");
+return -1;
+}
+break;
+case ERR_COUP:
+printf("tu ne peut pas jouer un coup avant de participer !\n");
+return -1;
+break;
+case ERR_TYP:
+printf("Erreur dans le type de requete\n");
+return -1;
+break;
+default:
+break;
+}
+// Arrange Play protocol package
+// TODO ask the IA for a coup and get the result
+playRequest.idRequest = COUP;
+playRequest.typeCoup = POS_PION;
+playRequest.coul = rep.coul;
+square.lg = TROIS;
+square.col = B;
+playRequest.action.posPion = square;
+// Send play
+err = send(sock, &playRequest, sizeof(TCoupReq), 0);
+if (err <= 0)
+{
+perror("(client) erreur sur le send de coup ");
+shutdown(sock, SHUT_RDWR);
+close(sock);
+}
+if (playRequest.coul == BLANC)
+printf("(client) BLANC send play\n");
+else
+printf("(client) NOIR send play \n");
 
-    // Treat adversaire play
-    switch (opponentResponse.err)
-    {
-    case ERR_OK:
-      if (opponentResponse.validCoup == VALID)
-      {
-        printf("(Client) coup adverse bien jouee!! \n");
-        if (opponentResponse.typeCoup == POS_PION)
-        {
-          printf("(Client) il a fait un pos  \n");
-        }
-        else if (opponentResponse.propCoup == CONT)
-        {
-          printf("(Client) aucun gagne pour le moment! \n");
-        }
-        else if (opponentResponse.propCoup == GAGNE)
-        {
-          printf("(Client) the adverse is wenning! \n");
-        }
-      }
-      else
-      {
-        // TODO treat other cases
-      }
-      break;
-    case ERR_COUP:
-      printf("tu ne peut pas jouer un coup avant de participer !\n");
-      break;
-    case ERR_TYP:
-      printf("Erreur dans le type de requete\n");
-      break;
-    default:
-      break;
-    }
+// Receive validation of own play
+err = recv(sock, &ownResponse, sizeof(TCoupRep), 0);
+if (err <= 0)
+{
+perror("(Client) erreur dans la reception de resultats coup ");
+close(sock);
+return -20;
+}
 
-    printf("(Client) you are the first player! lancer un coup\n");
-    // Arrange Play protocol package
-    // TODO ask the IA for a coup and get the result
-    playRequest.idRequest = COUP;
-    playRequest.typeCoup = POS_PION;
-    playRequest.coul = rep.coul;
-    square.lg = TROIS;
-    square.col = B;
-    playRequest.action.posPion = square;
-    // Send play
-    err = send(sock, &playRequest, sizeof(TCoupReq), 0);
-    if (err <= 0)
-    {
-      perror("(client) erreur sur le send de coup ");
-      shutdown(sock, SHUT_RDWR);
-      close(sock);
-    }
-    printf("(client) send demande coup de jouer 1 OK \n");
-    // Receive validation of own play
-    err = recv(sock, &ownResponse, sizeof(TCoupRep), 0);
-    if (err <= 0)
-    {
-      perror("(Client) erreur dans la reception de resultats coup ");
-      close(sock);
-      return -20;
-    }
+// Treat validation of own Play Response
+switch (ownResponse.err)
+{
+case ERR_OK:
+if (ownResponse.validCoup == VALID)
+{
+printf("(Client) coup bien jouee!! \n");
+if (playRequest.typeCoup == POS_PION)
+{
+printf("(Client) Ive just made a position movement \n");
+}
+if (ownResponse.propCoup == CONT)
+{
+printf("(Client) Game continues! \n");
+}
+else if (ownResponse.propCoup == GAGNE)
+{
+printf("(Client)I am winning!\n");
+}
+}
+else
+{
+printf("(Client) Ive made a mistake!! \n");
+return -1;
+}
+break;
+case ERR_COUP:
+printf("tu ne peut pas jouer un coup avant de participer !\n");
+return -1;
+break;
+case ERR_TYP:
+printf("Erreur dans le type de requete\n");
+return -1;
+break;
+default:
+break;
+}
+printf("(client) GOING BACK TO TOP OF LOOP\n");
+printf("\n");
+}
 
-    // Treat Play Response
-    switch (ownResponse.err)
-    {
-    case ERR_OK:
-      if (opponentResponse.validCoup == VALID)
-      {
-        printf("(Client) coup bien jouee!! \n");
-        if (opponentResponse.typeCoup == POS_PION)
-        {
-          printf("(Client) il a fait un pos \n");
-        }
-        else if (opponentResponse.propCoup == CONT)
-        {
-          printf("(Client) aucun gagne pour le moment! \n");
-        }
-        else if (opponentResponse.propCoup == GAGNE)
-        {
-          printf("(Client) the is wenning! \n");
-        }
-      }
-      else
-      {
-        // TODO treat other cases
-      }
-      break;
-    case ERR_COUP:
-      printf("tu ne peut pas jouer un coup avant de participer !\n");
-      break;
-    case ERR_TYP:
-      printf("Erreur dans le type de requete\n");
-      break;
-    default:
-      break;
-    }
+close(sock);
 
-    printf("\n");
-  }
-
-  close(sock);
-
-  return 0;
+return 0;
 }
--- serveurTCP.c
+++ serveurTCP.c
@@ -7,236 +7,238 @@
 
 int main(int argc, char **argv)
 {
-  int sockConx, /* descripteur socket connexion */
-      port,     /* numero de port */
-      sizeAddr; /* taille de l'adresse d'une socket */
+int sockConx, /* descripteur socket connexion */
+port, /* numero de port */
+sizeAddr; /* taille de l'adresse d'une socket */
 
-  int sockTrans[MAX_CLIENT];
-  int acc = 0;
+int sockTrans[MAX_CLIENT];
+int acc = 0;
 
-  int nPlays0 = 0;
-  int nPlays1 = 0;
+int nPlays0 = 0;
+int nPlays1 = 0;
 
-  struct sockaddr_in addClient; /* adresse de la socket client connectee */
+struct sockaddr_in addClient; /* adresse de la socket client connectee */
 
-  /*
-   * verification des arguments
-   */
-  if (argc != 2)
-  {
-    printf("usage : %s port\n", argv[0]);
-    return -1;
-  }
+/*
+* verification des arguments
+*/
+if (argc != 2)
+{
+printf("usage : %s port\n", argv[0]);
+return -1;
+}
 
-  port = atoi(argv[1]);
-  sockConx = socketServeur(port);
-  sizeAddr = sizeof(struct sockaddr_in);
+port = atoi(argv[1]);
+sockConx = socketServeur(port);
+sizeAddr = sizeof(struct sockaddr_in);
 
-  char nom1[MAXB];
-  char nom2[MAXB];
+char nom1[MAXB];
+char nom2[MAXB];
 
-  TIdRequest Idreq;
-  TPartieReq Preq;
-  TPartieRep Prep;
+TIdRequest Idreq;
+TPartieReq Preq;
+TPartieRep Prep;
 
-  bool gameon = true;
-  while (gameon)
-  {
-    // attente de connexion
-    int err;
-    while (acc < MAX_CLIENT)
-    {
+bool gameon = true;
+while (gameon)
+{
+// attente de connexion
+int err;
+while (acc < MAX_CLIENT)
+{
 
-      sockTrans[acc] = accept(sockConx, (struct sockaddr *)&addClient, (socklen_t *)&sizeAddr);
-      if (sockTrans[acc] < 0)
-      {
-        perror("(serveurTCP) erreur sur accept");
-        return -5;
-      }
+sockTrans[acc] = accept(sockConx, (struct sockaddr *)&addClient, (socklen_t *)&sizeAddr);
+if (sockTrans[acc] < 0)
+{
+perror("(serveurTCP) erreur sur accept");
+return -5;
+}
 
-      acc++;
+acc++;
 
-      printf("acc = %d \n", acc);
-    }
+printf("acc = %d \n", acc);
+}
+printf("(serveur) exit attente de connexion\n");
+// Receive of participate
+int i = 0;
+while (i < MAX_CLIENT)
+{
+err = recv(sockTrans[i], &Idreq, sizeof(TIdRequest), MSG_PEEK);
+if (err > 0)
+{
+printf("(serveur) recu demande parti OK de client %d \n", i);
+}
+switch (Idreq)
+{
+case PARTIE:
+err = recv(sockTrans[i], &Preq, sizeof(TPartieReq), 0);
 
-    // Receive of participate
-    int i = 0;
-    while (i < MAX_CLIENT)
-    {
-      err = recv(sockTrans[i], &Idreq, sizeof(TIdRequest), MSG_PEEK);
-      if (err > 0)
-      {
-        printf("(serveur) recu demande parti OK  de client %d \n", i);
-      }
-      switch (Idreq)
-      {
-      case PARTIE:
-        err = recv(sockTrans[i], &Preq, sizeof(TPartieReq), 0);
+if (err > 0)
+{
+printf("(serveur) recu complet de demande de parti de client %d \n", i);
+}
+else
+{
+printf("(serveur) erreur recu complet de parti de client %d \n", i);
+}
 
-        if (err > 0)
-        {
-          printf("(serveur) recu complet de demande de parti de client %d \n", i);
-        }
-        else
-        {
-          printf("(serveur) erreur recu complet de parti de client %d \n", i);
-        }
+if (i == 0)
+{
 
-        if (i == 0)
-        {
+memcpy(nom1, Preq.nomJoueur, sizeof nom1);
+printf("(serveurTCP) voila le nom recu : %s\n", nom1);
+}
 
-          memcpy(nom1, Preq.nomJoueur, sizeof nom1);
-          printf("(serveurTCP) voila le nom recu : %s\n", nom1);
-        }
+if (i == 1)
+{
 
-        if (i == 1)
-        {
+memcpy(nom2, Preq.nomJoueur, sizeof nom2);
+printf("(serveurTCP) voila le nom recu : %s\n", nom2);
+}
 
-          memcpy(nom2, Preq.nomJoueur, sizeof nom2);
-          printf("(serveurTCP) voila le nom recu : %s\n", nom2);
-        }
+break;
+case COUP:
+break;
+}
+i++;
+}
+printf("(serveur) exit receive of participate\n");
+// Response to the participants
+int j = 0;
+while (j < MAX_CLIENT)
+{
 
-        break;
-      case COUP:
-        break;
-      }
-      i++;
-    }
+if (j == 0)
+{
 
-    // Response to the participants
-    int j = 0;
-    while (j < MAX_CLIENT)
-    {
+printf("(serveur) waitt !%d \n", j);
+Prep.err = ERR_OK;
+Prep.coul = BLANC;
 
-      if (j == 0)
-      {
+memcpy(Prep.nomAdvers, nom2, sizeof Prep.nomAdvers);
 
-        printf("(serveur) waitt !%d \n", j);
-        Prep.err = ERR_OK;
-        Prep.coul = BLANC;
+printf("(serveur) le nom adverse est %s\n", Prep.nomAdvers);
+err = send(sockTrans[j], &Prep, sizeof(TPartieRep), 0);
+if (err > 0)
+printf("(serveur) send complet de demande de parti de client %d \n", j);
+j++;
+}
 
-        memcpy(Prep.nomAdvers, nom2, sizeof Prep.nomAdvers);
+if (j == 1)
+{
+Prep.err = ERR_OK;
+Prep.coul = NOIR;
+memcpy(Prep.nomAdvers, nom1, sizeof Prep.nomAdvers);
+printf("(serveur) le nom adverse est %s\n", Prep.nomAdvers);
+err = send(sockTrans[j], &Prep, sizeof(TPartieRep), 0);
+if (err > 0)
+{
+printf("(serveur) send complet de demande de parti de client %d \n", j);
+}
+else
+{
+printf("(serveur) send recu complet de parti de client %d \n", j);
+}
+}
 
-        printf("(serveur) le nom adverse est %s\n", Prep.nomAdvers);
-        err = send(sockTrans[j], &Prep, sizeof(TPartieRep), 0);
-        if (err > 0)
-          printf("(serveur) send complet de demande de parti de client %d \n", j);
-        j++;
-      }
+j++;
+}
+printf("(serveur) exit Response to the participants\n");
 
-      if (j == 1)
-      {
-        Prep.err = ERR_OK;
-        Prep.coul = NOIR;
-        memcpy(Prep.nomAdvers, nom1, sizeof Prep.nomAdvers);
-        printf("(serveur) le nom adverse est %s\n", Prep.nomAdvers);
-        err = send(sockTrans[j], &Prep, sizeof(TPartieRep), 0);
-        if (err > 0)
-        {
-          printf("(serveur) send complet de demande de parti de client %d \n", j);
-        }
-        else
-        {
-          printf("(serveur) send recu complet de parti de client %d \n", j);
-        }
-      }
+// Start Game
+int match = 0;
+int turn = 0;
+while (match < 2)
+{
+printf("(serveur) initialiserPartie\n");
+initialiserPartie();
+while (nPlays0 < 20 && nPlays1 < 20)
+{
+// receive and response
+TCoupReq request1;
+TCoupRep response1;
+TPropCoup propCoup;
+bool verif;
 
-      j++;
-    }
+// receive coup
+printf("(serveur) Attente de la reception de la requete COUP du J1\n");
+err = recv(sockTrans[turn], &request1, sizeof(TCoupReq), 0);
+if (err < 0)
+{
+perror("(serveur) erreur sur la reception de la requete COUP du J1 ");
+return -10;
+}
 
-    // Start Game
-    int match = 0;
-    int turn = 0;
-    while (match < 2)
-    {
-      initialiserPartie();
-      while (nPlays0 < 20 && nPlays1 < 20)
-      {
-        // receive and response
-        TCoupReq request1;
-        TCoupRep response1;
-        TPropCoup propCoup;
-        bool verif;
+// validate coup
+verif = validationCoup(turn+1, request1, &propCoup);
+printf("(serveur) Just verified pos\n");
+// Create response Crep COUP du J1
+response1.err = ERR_OK;
+response1.propCoup = propCoup;
 
-        // receive coup
-        printf("(serveur) Attente de la reception de la requete COUP du J1\n");
-        err = recv(sockTrans[turn], &request1, sizeof(TCoupReq), 0);
-        if (err < 0)
-        {
-          perror("(serveur) erreur sur la reception de la requete COUP du J1 ");
-          return -10;
-        }
+switch (response1.propCoup)
+{
+case NULLE:
+printf("(serveur) resultats est egeau\n");
+response1.validCoup = TIMEOUT;
+break;
 
-        // validate coup
-        verif = validationCoup(1, request1, &propCoup);
+case GAGNE:
+printf("(serveur) the first player gagne\n");
+response1.validCoup = VALID;
+match++;
+break;
 
-        // Create response Crep COUP du J1
-        response1.err = ERR_OK;
-        response1.propCoup = propCoup;
+case PERDU:
+printf("(serveur) the seconde player gagne\n");
+response1.validCoup = TRICHE;
+match++;
+break;
 
-        switch (response1.propCoup)
-        {
-        case NULLE:
-          printf("(serveur) resultats est egeau\n");
-          response1.validCoup = TIMEOUT;
-          break;
+case CONT:
+printf("(serveur) no one won the game!!\n");
+response1.validCoup = VALID;
+break;
+}
 
-        case GAGNE:
-          printf("(serveur) the first player gagne\n");
-          response1.validCoup = VALID;
-          match++;
-          break;
+// send response to players
+for (int i = 0; i < 2; i++)
+{
+printf("(serveur) Envoie de la reponse COUP\n");
+err = send(sockTrans[i], &response1, sizeof(TCoupRep), 0);
+if (err < 0)
+{
+perror("(serveur) erreur sur l'envoi de la reponse COUP a J" + i);
+return -11;
+}
+}
 
-        case PERDU:
-          printf("(serveur) the seconde player gagne\n");
-          response1.validCoup = TRICHE;
-          match++;
-          break;
+if (turn == 0)
+{
+nPlays0++;
+turn = 1;
+}
+else
+{
+nPlays1++;
+turn = 0;
+}
+}
+turn = 1;
+}
+int n = 0;
+printf("\n");
+}
 
-        case CONT:
-          printf("(serveur) no one won the game!!\n");
-          response1.validCoup = VALID;
-          break;
-        }
+/*
+* arret de la connexion et fermeture
+*/
+for (int i = 0; i < MAX_CLIENT; i++)
+{
+shutdown(sockTrans[i], SHUT_RDWR);
+close(sockTrans[i]);
+close(sockConx);
+}
 
-        // send response to players
-        for (int i = 0; i < 2; i++)
-        {
-          printf("(serveur) Envoie de la reponse COUP\n");
-          err = send(sockTrans[i], &response1, sizeof(TCoupRep), 0);
-          if (err < 0)
-          {
-            perror("(serveur) erreur sur l'envoi de la reponse COUP a J" + i);
-            return -11;
-          }
-        }
-
-        if (turn == 0)
-        {
-          nPlays0++;
-          turn = 1;
-        }
-        else
-        {
-          nPlays1++;
-          turn = 0;
-        }
-      }
-      turn = 1;
-    }
-    int n = 0;
-    printf("\n");
-  }
-
-  /*
-   * arret de la connexion et fermeture
-   */
-  for (int i = 0; i < MAX_CLIENT; i++)
-  {
-    shutdown(sockTrans[i], SHUT_RDWR);
-    close(sockTrans[i]);
-    close(sockConx);
-  }
-
-  return 0;
+return 0;
 }
