2a877227a6b4f8b4d77542194756e74b04667593
farahBaradi <farah.barade@gmail.com>
2022-05-08 14:27:28 +0200

    conflit

--- ColonnesDeTrois/src/main/java/game/Square.java
+++ ColonnesDeTrois/src/main/java/game/Square.java
@@ -7,7 +7,7 @@ import utils.*;
 public class Square {
     private List<Piece> pieces;
     private int size;
-    int thiago;
+    String thiago ;
 
     public Square() {
         pieces = new ArrayList<Piece>(Utils.MAX_PILE_SIZE);
